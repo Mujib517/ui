c705c1fb39581d331bf80e388aa06152c2e5855d
Mujib517 <Mujib517@gmail.com>
2018-10-10 08:08:23 +0530

    JS

---
+++ scripts/index.js
@@ -0,0 +1,57 @@
+// user input validation
+// 7 years angularjs, react
+// web, mobile, Desktop, IOT, ML, AI
+// ES
+// Loosely typed lang dynamically 
+// Java C# strongly typed
+// variables
+// primary 4GB, secondary 500GB
+// cache 5MB
+// data types
+//function is a value
+// typeof
+// functions
+// max 6 lines of code
+// input parameters
+// no params: ideal, 3: good
+// output params 
+// CPL 
+
+// //function expression
+// var fun = function () {
+//   console.log('fadkfjkadjf');
+// }
+
+// // fun();
+// //fun declaration
+// function fun() {
+//   console.log("fkadjfkajdkjf");
+// }
+
+
+
+// function add(a, b) {
+//   var c = a + b;
+//   return c;
+// }
+
+// var res = add(10, 20);
+// console.log(res);
+
+// var x = 100;  //number
+// var str = "abc"; //string
+// var acceptLicense = true; //boolean
+// var y = null; 
+// var z; //undefined
+// var obj={}; //object
+// var fun=function(){} //function
+
+
+// console.log(typeof y);  //Object
+
+// console.log(typeof x); 
+// console.log(typeof str); 
+// console.log(typeof acceptLicense); 
+
+// console.log(typeof obj); 
+// console.log(typeof fun); 

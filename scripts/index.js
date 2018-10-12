6b1f1052564ab94f9bfe43fb5d7d106b12a6ab85
Mujib517 <Mujib517@gmail.com>
2018-10-12 08:01:37 +0530

    Loops:

--- scripts/index.js
+++ scripts/index.js
@@ -1,60 +1,3 @@
-// user input validation
-// 7 years angularjs, react
-// web, mobile, Desktop, IOT, ML, AI
-// ES
-// Loosely typed lang dynamically 
-// Java C# strongly typed
-// variables
-// primary 4GB, secondary 500GB
-// cache 5MB
-// data types
-//function is a value
-// typeof
-// functions
-// max 6 lines of code
-// input parameters
-// no params: ideal, 3: good
-// output params 
-// CPL 
-
-// //function expression
-// var fun = function () {
-//   console.log('fadkfjkadjf');
-// }
-
-// // fun();
-// //fun declaration
-// function fun() {
-//   console.log("fkadjfkajdkjf");
-// }
-
-
-
-// function add(a, b) {
-//   var c = a + b;
-//   return c;
-// }
-
-// var res = add(10, 20);
-// console.log(res);
-
-// var x = 100;  //number
-// var str = "abc"; //string
-// var acceptLicense = true; //boolean
-// var y = null; 
-// var z; //undefined
-// var obj={}; //object
-// var fun=function(){} //function
-
-
-// console.log(typeof y);  //Object
-
-// console.log(typeof x); 
-// console.log(typeof str); 
-// console.log(typeof acceptLicense); 
-
-// console.log(typeof obj); 
-// console.log(typeof fun); 
 //function expression function declaration
 // not compiled --> intermediate outcome
 // java --> .class --> output
@@ -64,65 +7,51 @@
 // void
 // method function
 // no void method
-// Operators +,-,*,/,%, ++, --
+// Arithmatic Operators +,-,*,/,%, ++, --
 // conditonal stmts if,else, switch, ternary 
 // truthy falsy
+// conditional operators <,>,==, ===, <=, >=, !=, !==
+// logical operators &&, ||, ! Logical AND
+// loops:for,while, do while
+// continue, break
+// loop variant 1 2 3 .... 100 101
+// i=1; 1<=10 true, 2, 2<=100  ..... i=100, 100<=100, i=101 101<=100
+// for (var i = 1; i <= 5;i++) {
+//   console.log(i);
+// }
+// strict equality check
+// arrays
 
-var isYoung = {};
+//odd numbers
+// 100
+// assignment: write a function which takes a number and returns whether that numbers is prime
+//shorthand evaluation
+/*
+  Truth tables
 
-if (isYoung) {  // 0, undefined, null, "", false, NaN
-  console.log("If block");
+  a   b   a&b    a|b    !a
+  ============================
+  0   0    0      0      1
+  0   1    0      1      1
+  1   0    0      1      0
+  1   1    1      1      0
+
+*/
+
+var marks = [20, 50, 100, 150];
+
+marks[6] = 200;
+
+for (var i = 0; i < marks.length; i++) {
+  console.log(marks[i]);
 }
-else {
-  console.log("Else block");
-}
 
 
-// var isYoung;
-// var age = 30; //assignment
+//strings, for in , objects
 
-// isYoung = age <= 35 ? true : false;
+console.log("orignal array", marks);
 
-
-// if (age <= 35) {
-//   isYoung = true;
-// }
-// else {
-//   isYoung = false;
+// for (var i = 0; i < marks.length; i++) {
+//   console.log(marks[i]);
 // }
 
-// console.log("you are young", isYoung == true ? "Yes" : "No");
-
-
-
-
-
-
-// if (age == 10) { //comparision
-//   console.log("You are kid");
-// }
-// else if (age == 20) {
-//   console.log("You are young");
-// }
-// else if (age == 30) {
-//   console.log("You are still young");
-// }
-// else {
-//   console.log("You rock");
-// }
-
-
-// switch (age) {
-//   case 10:
-//     console.log("You are kid");
-//     break;
-//   case 20:
-//     console.log("You are young");
-//     break;
-//   case 30:
-//     console.log("You are still young");
-//     break;
-//   default:
-//     console.log("You rock");
-//     break;
-// }

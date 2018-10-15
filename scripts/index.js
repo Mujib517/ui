2e079676f90ce1f804fbd44686e68ee6c6a8e25d
Mujib517 <Mujib517@gmail.com>
2018-10-15 08:00:40 +0530

    Objects

--- scripts/index.js
+++ scripts/index.js
@@ -1,57 +1,16 @@
-//function expression function declaration
-// not compiled --> intermediate outcome
-// java --> .class --> output
-// javascript --> output
-// C# --> .exe --> output
-// hoisting
-// void
-// method function
-// no void method
-// Arithmatic Operators +,-,*,/,%, ++, --
-// conditonal stmts if,else, switch, ternary 
-// truthy falsy
-// conditional operators <,>,==, ===, <=, >=, !=, !==
-// logical operators &&, ||, ! Logical AND
-// loops:for,while, do while
-// continue, break
-// loop variant 1 2 3 .... 100 101
-// i=1; 1<=10 true, 2, 2<=100  ..... i=100, 100<=100, i=101 101<=100
-// for (var i = 1; i <= 5;i++) {
-//   console.log(i);
-// }
-// strict equality check
-// arrays
+// constructor function
+// Pascal casing
+function Mobile(color) {
+  this.width = 5;
+  this.height = 10;
+  this.color = color;
 
-//odd numbers
-// 100
-// assignment: write a function which takes a number and returns whether that numbers is prime
-//shorthand evaluation
-/*
-  Truth tables
-
-  a   b   a&b    a|b    !a
-  ============================
-  0   0    0      0      1
-  0   1    0      1      1
-  1   0    0      1      0
-  1   1    1      1      0
-
-*/
-
-var marks = [20, 50, 100, 150];
-
-marks[6] = 200;
-
-for (var i = 0; i < marks.length; i++) {
-  console.log(marks[i]);
+  this.call = function (number) {
+    console.log("Calling", number);
+  }
 }
 
-
-//strings, for in , objects
-
-console.log("orignal array", marks);
-
-// for (var i = 0; i < marks.length; i++) {
-//   console.log(marks[i]);
-// }
+var m1 = new Mobile("Red");
+var m2 = new Mobile("Black");
+var m3 = new Mobile("Blue");
 
---
+++ scripts/index2.js
@@ -0,0 +1,94 @@
+//function expression function declaration
+// not compiled --> intermediate outcome
+// java --> .class --> output
+// javascript --> output
+// C# --> .exe --> output
+// hoisting
+// void
+// method function
+// no void method
+// Arithmatic Operators +,-,*,/,%, ++, --
+// conditonal stmts if,else, switch, ternary 
+// truthy falsy
+// conditional operators <,>,==, ===, <=, >=, !=, !==
+// logical operators &&, ||, ! Logical AND
+// loops:for,while, do while
+// continue, break
+// loop variant 1 2 3 .... 100 101
+// i=1; 1<=10 true, 2, 2<=100  ..... i=100, 100<=100, i=101 101<=100
+// for (var i = 1; i <= 5;i++) {
+//   console.log(i);
+// }
+// strict equality check
+// arrays
+
+//odd numbers
+// 100
+// assignment: write a function which takes a number and returns whether that numbers is prime
+//shorthand evaluation
+//Strings
+
+// var str = "abcdef";
+// str = str + "ghi";
+
+// var url = "www.google.com/gmail";
+
+// var start = url.indexOf("/");
+// console.log(url.length - start - 1);
+
+// var tokens = url.split("/");
+// console.log(tokens);
+// console.log(tokens[1].length);
+// 0, false, "",undefined, NaN, null
+
+// var str3 = "abc";   //truthy --> !truthy-- falsy --> !falsty ->truthy
+
+// var res = !!str3;
+// var res;
+
+// if (str3) res = true;
+// else res = false;
+
+// var res = str3 ? true : false;
+
+// console.log(res, typeof res);
+
+
+
+// var x = 'abc';
+// var y = +x;  //x+=x;
+// var z= parseInt(x);
+// console.log(typeof x, typeof y, x, y,z);
+
+//str+="ghi";
+//var res = str.slice(3, 6);
+// var idx = str.indexOf("abc");
+// if (idx > -1) {
+//   console.log("Found");
+// }
+// else {
+//   console.log("Not found");
+// }
+
+
+// var sub = str.substr(3, 10);
+// console.log(sub);
+
+// var str2 = "abc,def,ghi,kjm";  // abc def ghi kjm
+// var arr = str2.split(",");
+// console.log(arr);
+
+// for (var i = 0; i < str.length; i++) {
+//   console.log(str[i]);
+// }
+
+// var width = 5;
+// var height = 10;
+// var color = "green";
+//4 ways:Object literal
+
+var mobile = {
+  width: 5,
+  height: 10,
+  color: 'red'
+};
